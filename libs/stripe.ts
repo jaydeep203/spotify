0b707fe0691f4cf57412961f0d1f5e5eb792722d
Jaydeep Deshpande <mrjaydeep2003@gmail.com>
2023-08-24 16:40:21 +0530

    stripe fixed

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 export const stripe = new Stripe(
     process.env.STRIPE_SECRET_KEY ?? "",
     {
-        apiVersion: "2023-08-16",
+        apiVersion: "2022-11-15",
         appInfo:{
             name:"Spotify clone",
             version: "0.1.0"
